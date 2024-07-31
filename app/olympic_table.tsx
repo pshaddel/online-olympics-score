697e1d6438308609fbc33193f59062df31273fa5
Poorshad Shaddel <poorshad.shaddel@redbull.com>
2024-07-31 15:33:06 +0200

    make it better for bigger screens

--- app/olympic_table.tsx
+++ app/olympic_table.tsx
@@ -11,38 +11,39 @@ export default function OlympicTable({ data }: {
       window.location.reload();
     }, 60000);
   }, []);
+  const elemsize = "px-6 py-6"
   return (
-    <div className="overflow-x-auto">
-    <table className="w-full table-auto">
-      <thead>
-        <tr>
-          <th className="px-2 py-2 min-w-9"></th>
-          <th className="px-2 py-2">Country</th>
-          <th className="px-2 py-2 text-yellow-500">Gold</th>
-          <th className="px-2 py-2 text-gray-400">Silver</th>
-          <th className="px-2 py-2 text-orange-500">Bronze</th>
-          <th className="px-2 py-2">Total</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((row, index) => (
+    // make font size of everything 20% bigger
+    <div className="overflow-x-auto text-lg">
+      <table className="min-w-full table-auto">
+        <thead>
+          <tr>
+            <th className={`${elemsize} min-w-9`}>Logo</th>
+            <th className={`${elemsize}`}>Country</th>
+            <th className={`${elemsize} text-yellow-500`}>Gold</th>
+            <th className={`${elemsize} text-gray-400`}>Silver</th>
+            <th className={`${elemsize} text-orange-500`}>Bronze</th>
+            <th className={`${elemsize}`}>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((row, index) => (
             <tr key={index}>
-                <td className="border px-2 py-2"><img
-                    src={row[0]}
-                    alt={`${row[1]} Flag`}
-                    width={22}
-                    height={15}
-                /></td>
-                <td className="border px-2 py-2">{row[1]}</td>
-                <td className="border px-2 py-2 text-yellow-500">{row[3]}</td>
-                <td className="border px-2 py-2 text-gray-400" >{row[4]}</td>
-                <td className="border px-2 py-2 text-orange-500">{row[5]}</td>
-                <td className="border px-2 py-2">{row[6]}</td>
+              <td className={`border ${elemsize}`}><img
+                src={row[0]}
+                alt={`${row[1]} Flag`}
+                width={22}
+                height={15}
+              /></td>
+              <td className={`border ${elemsize}`}>{row[1]}</td>
+              <td className={`border ${elemsize} text-yellow-500`}>{row[3]}</td>
+              <td className={`border ${elemsize} text-gray-400`}>{row[4]}</td>
+              <td className={`border ${elemsize} text-orange-500`}>{row[5]}</td>
+              <td className={`border ${elemsize}`}>{row[6]}</td>
             </tr>
-
-        ))}
-      </tbody>
-    </table>
-  </div>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,14 @@ import { Board } from "./board";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between p-24 mt-8">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex mb-2">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+    <main className="flex flex-col items-center justify-between p-24 mt-20">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200  pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           <Image
             src="/logo.png"
             alt="Red Bull Logo"
-            width={100}
-            height={24}
+            width={200}
+            height={60}
             priority
           />
         </p>
